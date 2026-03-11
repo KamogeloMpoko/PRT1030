a30ad987c8208c009b7ef3ea65f7d53f4591a933
Kamogelo Mpoko <112622646+KamogeloMpoko@users.noreply.github.com>
2026-03-11 17:31:14 +0200

    Add files via upload

---
+++ second year PRT1030/Prac2/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical2_PRT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows, columns;
+            int rowSum, columnSum;
+
+            Console.WriteLine("Enter number of rows:");
+            rows = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter number of columns:");
+            columns = int.Parse(Console.ReadLine());
+
+            int[,] numbers = new int[rows, columns];
+
+            // Fill the array
+
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < columns; c++)
+                {
+                    Console.WriteLine("Enter a number for row " + r + " and column " + c);
+                    numbers[r, c] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            // Display the array
+
+            Console.WriteLine("\nArray:");
+
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < columns; c++)
+                {
+                    Console.Write(numbers[r, c] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            // Row sums
+
+            for (int r = 0; r < rows; r++)
+            {
+                rowSum = 0;
+
+                for (int c = 0; c < columns; c++)
+                {
+                    rowSum += numbers[r, c];
+                }
+
+                Console.WriteLine("The sum of row " + r + " is " + rowSum);
+            }
+
+            // Column sums
+
+            for (int c = 0; c < columns; c++)
+            {
+                columnSum = 0;
+
+                for (int r = 0; r < rows; r++)
+                {
+                    columnSum += numbers[r, c];
+                }
+
+                Console.WriteLine("The sum of column " + c + " is " + columnSum);
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+}
