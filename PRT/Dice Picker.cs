0ca2ad3eb316a8a149417fe0dba914b9aeeb4ed0
Kamogelo Mpoko <112622646+KamogeloMpoko@users.noreply.github.com>
2026-04-14 20:50:49 +0200

    Add files via upload

---
+++ PRT/Dice Picker.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Dice_picker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[] dice = new int[2];
+            int totalRolls = 0;
+
+            for (int i = 0; i < dice.Length; i++)
+            {
+                dice[i] = random.Next(1, 7);
+                Console.WriteLine("Dice " + (i + 1) + ": " + dice[i]);
+            }
+
+            if (dice[0] == dice[1])
+            {
+                Console.WriteLine("You rolled a double! Both dice show: " + dice[0]);
+            }
+            else
+            {
+                Console.WriteLine("You rolled: " + dice[0] + " and " + dice[1]);
+            }
+
+            totalRolls = dice[0] + dice[1];
+            Console.WriteLine("Total: " + totalRolls);
+
+
+        }
+    }
+
+}
